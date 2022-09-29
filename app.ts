e38f9d69af900c6e44c8c28a9ee4ed3088e4f41f
psuman9492 <psuman9492@gmail.com>
2022-09-29 18:51:54 -0400

    done!

--- app.ts
+++ app.ts
@@ -1,1 +1,24 @@
-console.log("Welcome to Type-Script - Madan");
+const person = {
+  name: "Madan",
+  age: 27,
+  status: "F1",
+};
+
+console.log(person.name);
+
+// we can also specify the what are the types in object by
+
+const address: {
+  streetname: string;
+  housenumber: number;
+  zipcode: number;
+  city: string;
+} = {
+  streetname: "gayatrinagar",
+  housenumber: 42 - 32 - 1 / 2,
+  zipcode: 500040,
+  city: "hyderabad",
+};
+console.log(address.streetname);
+
+// but declaring the type in objects is unessary since type script will uatomatically assign the defacutl type by the value assigned.
