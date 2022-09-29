3e500b89c16403ac9ed7bfce4ae470ca60049fbb
psuman9492 <psuman9492@gmail.com>
2022-09-29 19:06:46 -0400

    done!

--- app.ts
+++ app.ts
@@ -1,1 +1,15 @@
-console.log("Welcome to Type-Script - Madan");
+// ARRAY-TYPE
+
+const person = {
+  name: "madan",
+  age: 28,
+  hobbies: ["cooking", "drawing"],
+  // now hobbies will be assigned type string[]
+};
+// we can print all the hobbies of the person
+
+for (const display of person.hobbies) {
+  console.log(display);
+  // we can use the avilable featires of string for the string[]
+  console.log(display.toUpperCase());
+}
