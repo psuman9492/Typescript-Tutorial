6c41e6eff055803882dda1052b3335db93e4499c
psuman9492 <psuman9492@gmail.com>
2022-09-28 22:27:02 -0400

    initial setup

--- app.ts
+++ app.ts
@@ -1,11 +1,1 @@
-const person = {
-  name: "madan",
-  age: 28,
-  hobbies: ["sketching", "cooking"],
-};
-
-console.log(person.name);
-
-for (const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase());
-}
+console.log("Welcome to Type-Script - Madan");
