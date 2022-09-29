369182e9f8863704cff25bec66e0c8c7877b3c9b
psuman9492 <psuman9492@gmail.com>
2022-09-28 22:42:50 -0400

    numbertype

---
+++ numbertype.ts
@@ -0,0 +1,9 @@
+function add(a: number, b: number) {
+  return a + b;
+}
+
+const number1 = 10;
+const number2 = 4;
+
+const additionvalue = add(number1, number2);
+console.log("the value of the summation is" + additionvalue);
